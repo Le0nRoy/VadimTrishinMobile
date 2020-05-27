e32e5a30a1a306d8063afb706019d33d5faee58b
le0n <cerber9283@gmail,com>
2020-05-27 22:56:51 +0300

    Added method to register in native app

--- src/main/java/pageObjects/NativePageObject.java
+++ src/main/java/pageObjects/NativePageObject.java
@@ -10,6 +10,20 @@ public class NativePageObject {
 
     @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/email_sign_in_button")
     WebElement signInBtn;
+    @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/register_button")
+    WebElement registerBtn;
+    @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/registration_email")
+    WebElement emailField;
+    @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/registration_username")
+    WebElement userNameField;
+    @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/registration_password")
+    WebElement passwordField;
+    @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/registration_confirm_password")
+    WebElement passwordConfirmationField;
+    @AndroidFindBy(xpath = "//android.widget.CheckedTextView")
+    WebElement agreamentsCheckbox;
+    @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/register_new_account_button")
+    WebElement registerNewAccountBtn;
 
     public NativePageObject(AppiumDriver appiumDriver) {
 
--- src/test/java/scenarios/nativeMobileTests.java
+++ src/test/java/scenarios/nativeMobileTests.java
@@ -8,9 +8,22 @@ public class nativeMobileTests extends BaseTest {
     @Test(groups = {"native"},
             description = "This simple test just click on the Sign In button")
     public void simpleNativeTest() throws IllegalAccessException, NoSuchFieldException, InstantiationException {
-        getPo().getWelement("signInBtn").click();
-        System.out.println("Simplest Android native test done");
 
+        getPo().getWelement("signInBtn").click();
+
+        registerAccount("example@email.com", "example", "justPass");
+        System.out.println("Simplest Android native test done");
     }
 
+    private void registerAccount(String email, String userName, String password) throws IllegalAccessException, NoSuchFieldException, InstantiationException {
+
+        getPo().getWelement("registerBtn").click();
+        getPo().getWelement("emailField").sendKeys(email);
+        getPo().getWelement("userNameField").sendKeys(userName);
+        getPo().getWelement("passwordField").sendKeys(password);
+        getPo().getWelement("passwordConfirmationField").sendKeys(password);
+        // FIXME not gets clicked
+        getPo().getWelement("agreamentsCheckbox").click();
+        getPo().getWelement("registerNewAccountBtn").click();
+    }
 }
