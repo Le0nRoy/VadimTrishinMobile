15506e325924ea57bc5138d0f3f55eba99800094
le0n <cerber9283@gmail,com>
2020-06-01 16:46:33 +0300

    Finished hw 2

--- src/main/java/pageObjects/webObjects/GooglePageObject.java
+++ src/main/java/pageObjects/webObjects/GooglePageObject.java
@@ -1,13 +1,19 @@
 package pageObjects.webObjects;
 
 import io.appium.java_client.AppiumDriver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
 import pageObjects.AbstractPageObject;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class GooglePageObject extends AbstractPageObject {
+
+    WebElement searchInput;
+    WebElement searchBtn;
+    List<WebElement> searchResults;
 
     public GooglePageObject(AppiumDriver appiumDriver) {
 
@@ -24,11 +30,21 @@ public class GooglePageObject extends AbstractPageObject {
 
     public void searchByKeyword(String keyword) {
 
+        searchInput = driver.findElement(By.xpath("//input[@name='q']"));
+        searchBtn = driver.findElement(By.xpath("//button[@class='Tg7LZd']"));
+
+        searchInput.sendKeys(keyword);
+        searchBtn.click();
     }
 
     public List<String> getSearchResults() {
 
         List<String> results = new ArrayList<>();
+
+        searchResults = driver.findElements(By.xpath("//div[contains(text(),'EPAM | Enterprise Software Development, Design & Consulting')]"));
+        for (int i = 0; i < searchResults.size(); ++i) {
+            results.add(searchResults.get(i).toString());
+        }
         return results;
     }
 }
